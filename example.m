f7fd3d483277bab50e6c937a0b7ee3bfa852186f
Hirokazu Ishida <spitfire.docomo@gmail.com>
2018-04-22 11:37:44 +0900

    test

---
+++ example.m
@@ -0,0 +1,29 @@
+A = [1 1; 0 1]; B = 0.5*[1; 2];
+Q = ones(2); R = 0.1; 
+F = [-0.1, 0;
+    0.1, 0;
+    0, -0.5;
+    0, 0.5;
+    0, 0;
+    0, 0];
+G = [0; 0; 0; 0; -1; 1];
+v = [0.5, 0.5; 0.5, -0.5; -0.5, -0.5; -0.5, 0.5]*0.5;
+W = Polyhedron('V', v);
+N = 10;
+Xc = Polyhedron(F(1:4, :), ones(4, 1));
+Uc = Polyhedron(G(5:6, :), ones(2, 1));
+mp = tube_mpc(A, B, Q, R, Xc, Uc, N, W);
+x_init = [8.6; 1];
+mp.init_x(x_init);
+clf
+for i=1:100
+    mp.propagate();
+end
+mp.show_Xc();
+mp.show_Xc_robust();
+mp.show_Xter();
+mp.show_Xter_robust();
+mp.show_tube();
+mp.show_init_nominal_traj();
+mp.show_real_traj();
+mp.show_auto_limit();
