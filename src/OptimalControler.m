2e9bc1b1da7776acb603952424dcd52041e14209
HiroIshida <spitfire.docomo@gmail.com>
2020-05-17 17:39:13 +0900

    remove ineq const members; fix comments

--- src/OptimalControler.m
+++ src/OptimalControler.m
@@ -1,4 +1,9 @@
 classdef OptimalControler < handle
+    % Let s :=[x(0), .., x(n), u(0)....u(n-1)] be decision variables, then
+    % optimal control problem is composed by
+    % (i) objective function V(s) = s'*H*S in quadratic form
+    % (ii) equality constraints specified by C_eq1 and C_eq2 where C_eq1*s =C_eq2
+    % (iii) inequality constraints specified by C_ineq1 and C_ineq2 where C_ineq1 * s <= C_ineq2
     
     properties (SetAccess = private)
         sys; %system
@@ -7,9 +12,7 @@ classdef OptimalControler < handle
         N; % prediction horizon
         Ak; % S.T.M of closed-roop system with LQR feedback
         n_opt; % dim. of optimization parameter
-        H; % quadratic const V to be minimized here will be expressed as V = s'*H*s, where s:=[x(0), .., x(n-1), x(n), u(0)....u(n-1)].
-        C_eq1; C_eq2; % equality constraints are defined as C_eq1*s =C_eq2; C_eq2 is a function object 
-        C_neq1; C_neq2; % inequality constraints are defined as C_neq*s<=1 (1 is a vector)
+        H; 
         constraint_manager;
     end
     
@@ -29,50 +32,32 @@ classdef OptimalControler < handle
             
             obj.H = obj.construct_costfunction();
             [C_eq1, C_eq2] = obj.construct_dynamics_constraint();
-            [C_neq1, C_neq2] = obj.construct_ineq_constraint(Xc, Uc);
-            obj.C_eq1 = C_eq1;
-            obj.C_eq2 = C_eq2;
-            obj.C_neq1 = C_neq1;
-            obj.C_neq2 = C_neq2;
+            [C_ineq1, C_ineq2] = obj.construct_ineq_constraint(Xc, Uc);
 
             %% Let's change initial and dynamics constraints!!
             obj.constraint_manager.add_eq_constraint('dynamics', C_eq1, C_eq2)
-            obj.constraint_manager.add_ineq_constraint('feasible', C_neq1, C_neq2)
+            obj.constraint_manager.add_ineq_constraint('feasible', C_ineq1, C_ineq2)
         end
 
-        function remove_initial_eq_constraint(obj)
-            % In tube MPC, the initial state of the nominal trjectory must not 
-            % be the actual initial state (see section 4 of Mayne et al, Robust model predictive control of constrained linear systems with bounded disturbances, 2005)
-            % this function will be used in tube model predictive control
-            obj.C_eq1 = obj.C_eq1(obj.sys.nx+1:size(obj.C_eq1, 1), 1:size(obj.C_eq1, 2));
-            obj.C_eq2 = @(x) zeros(size(obj.C_eq1, 1), 1);
-        end
-        
         function add_initial_eq_constraint(obj, x_init)
             % E * x0 = x_init
             idx_x0_start = 1
             idx_x0_end = obj.sys.nx 
 
-            C_initial_eq1 = zeros(obj.sys.nx, obj.n_opt)
-            C_initial_eq1(:, idx_x0_start:idx_x0_end) = eye(obj.sys.nx)
-            C_initial_eq2 = x_init
-
-            obj.C_eq1 = [obj.C_eq1; C_initial_eq1]
-            obj.C_eq2 = [obj.C_eq2; C_initial_eq2]
+            C_eq1_init = zeros(obj.sys.nx, obj.n_opt)
+            C_eq1_init(:, idx_x0_start:idx_x0_end) = eye(obj.sys.nx)
+            C_eq2_init = x_init
+            obj.constraint_manager.add_eq_constraint('initial', C_eq1_init, C_eq2_init)
         end
 
         function add_terminal_constraint(obj, Xadd)
-            [C_neq1_add, C_neq2_add] = add_ineq_constraint(obj, Xadd, obj.N+1);
-            obj.C_neq1 = [obj.C_neq1; C_neq1_add]
-            obj.C_neq2 = [obj.C_neq2; C_neq2_add]
-            obj.constraint_manager.add_ineq_constraint('terinal', C_neq1_add, C_neq2_add)
+            [C_ineq1_add, C_ineq2_add] = add_ineq_constraint(obj, Xadd, obj.N+1);
+            obj.constraint_manager.add_ineq_constraint('terinal', C_ineq1_add, C_ineq2_add)
         end
 
         function add_initial_constraint(obj, Xadd)
-            [C_neq1_add, C_neq2_add] = add_ineq_constraint(obj, Xadd, 1);
-            obj.C_neq1 = [obj.C_neq1; C_neq1_add]
-            obj.C_neq2 = [obj.C_neq2; C_neq2_add]
-            obj.constraint_manager.add_ineq_constraint('initial', C_neq1_add, C_neq2_add)
+            [C_ineq1_add, C_ineq2_add] = add_ineq_constraint(obj, Xadd, 1);
+            obj.constraint_manager.add_ineq_constraint('initial', C_ineq1_add, C_ineq2_add)
         end
         
         function [x_seq, u_seq] = solve(obj)
@@ -142,8 +127,8 @@ classdef OptimalControler < handle
             C_eq2 = zeros(size(C_eq1, 1), 1) 
         end
        
-        function [C_neq1, C_neq2] = construct_ineq_constraint(obj, Xc, Uc)
-            % compute C_neq
+        function [C_ineq1, C_ineq2] = construct_ineq_constraint(obj, Xc, Uc)
+            % compute C_ineq
             [F, G, nc] = convert_Poly2Mat(Xc, Uc);
             
             F_block = [];
@@ -154,29 +139,29 @@ classdef OptimalControler < handle
             for itr = 1:obj.N+1
                 F_block = blkdiag(F_block, F);
             end
-            C_neq1 = [F_block, [G_block; zeros(nc, obj.sys.nu*obj.N)]];
-            nc_total = size(C_neq1, 1);
-            C_neq2 = ones(nc_total, 1);
+            C_ineq1 = [F_block, [G_block; zeros(nc, obj.sys.nu*obj.N)]];
+            nc_total = size(C_ineq1, 1);
+            C_ineq2 = ones(nc_total, 1);
         end
         
     end
     
     methods (Access = private)
         
-        function [C_neq1_add, C_neq2_add] = add_ineq_constraint(obj, Xadd, k_add)
+        function [C_ineq1_add, C_ineq2_add] = add_ineq_constraint(obj, Xadd, k_add)
             % add a new constraint at time step k 
             if Xadd.contains(zeros(2, 1)) % If Xadd contains the origin, the contraint can be expressed as C1*x<=1
                 [F_add, ~, nc_add] = convert_Poly2Mat(Xadd, Polyhedron());
-                C_neq2_add = ones(nc_add, 1)
+                C_ineq2_add = ones(nc_add, 1)
                 
             else % in other cases, expressed in a general affine form C1*x<=C2
                 F_add = Xadd.A;
                 nc_add = size(F_add, 1);
-                C_neq2_add = Xadd.b
+                C_ineq2_add = Xadd.b
             end
             
-            C_neq1_add = zeros(nc_add, obj.n_opt);
-            C_neq1_add(:, (k_add-1)*obj.sys.nx+1:k_add*obj.sys.nx) = F_add;
+            C_ineq1_add = zeros(nc_add, obj.n_opt);
+            C_ineq1_add(:, (k_add-1)*obj.sys.nx+1:k_add*obj.sys.nx) = F_add;
         end
         
     end
