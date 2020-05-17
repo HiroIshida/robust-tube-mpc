f01765790259a897ab6fdbf59327aa22481951a1
HiroIshida <spitfire.docomo@gmail.com>
2020-05-17 15:06:10 +0900

    add semicolon

--- src/ConstraintManager.m
+++ src/ConstraintManager.m
@@ -1,31 +1,31 @@
 classdef ConstraintManager < handle
 
     properties (SetAccess = private)
-        C_equality
-        C_inequality
+        C_equality;
+        C_inequality;
     end
 
     methods (Access = public)
 
         function obj = ConstraintManager()
-            obj.C_equality = containers.Map()
-            obj.C_inequality = containers.Map()
+            obj.C_equality = containers.Map();
+            obj.C_inequality = containers.Map();
         end
 
         function add_eq_constraint(obj, key, C_eq1, C_eq2)
-            obj.C_equality(key) =  {C_eq1, C_eq2}
+            obj.C_equality(key) =  {C_eq1, C_eq2};
         end
 
         function add_neq_constraint(obj, key, C_neq1, C_neq2)
-            obj.C_inequality(key) =  {C_neq1, C_neq2}
+            obj.C_inequality(key) =  {C_neq1, C_neq2};
         end
 
         function remove_eq_constraint(obj, key)
-            remove(obj.C_equality, key)
+            remove(obj.C_equality, key);
         end
 
         function remove_neq_constraint(obj, key)
-            remove(obj.C_inequality, key)
+            remove(obj.C_inequality, key);
         end
     end
 end
