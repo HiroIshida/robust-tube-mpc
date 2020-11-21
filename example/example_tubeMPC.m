508a00563b49d1d9c2409368e10339bd952c7c2c
HiroIshida <spitfire.docomo@gmail.com>
2020-11-21 16:52:10 +0900

    string -> char
    in lower version of matlab, saveas seems to not accept string

--- example/example_tubeMPC.m
+++ example/example_tubeMPC.m
@@ -31,7 +31,7 @@ mpc = TubeModelPredictiveControl(disturbance_system, Xc, Uc, N_horizon);
 
 % The robust MPC guidances the path inside the robust MPI-set so that the path will reach the robust MPI-set in N_horizon. 
 x = [-7; -2];
-savedir_name = 'results';
+savedir_name = './results/';
 mkdir(savedir_name);
 
 for i = 1:15
@@ -39,7 +39,8 @@ for i = 1:15
     u_next = mpc.solve(x);
     x = disturbance_system.propagate(x, u_next); % additive disturbance is considered inside the method 
     mpc.show_prediction();
-    saveas(gcf, strcat(savedir_name, '/tmpc_seq', number2string(i), '.png')); % removing this line makes the code much faster
+    filename = strcat(savedir_name, 'tmpc_seq', number2string(i), '.png')
+    saveas(gcf, char(filename)); % removing this line makes the code much faster
     clf;
 end
 
